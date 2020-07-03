dda412515819ab4f17187be1bb90c9a8e7378625
Thiago Stahlschmidt <thiago@MacBook-Pro-de-Thiago.local>
2020-07-03 13:49:58 -0300

    run on windows wsl2 (no xhost)

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 set -e
 docker build -t ibank:sandbox .
 docker run --privileged -dt --shm-size=2G --rm --name ibanksandbox -e DISPLAY=host.docker.internal:0 ibank:sandbox
-xhost +127.0.0.1
+xhost +127.0.0.1 &> /dev/null || true
 docker exec --user bankusr -d ibanksandbox /bin/bash -c browser
