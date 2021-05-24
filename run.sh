a4a100155b1b3dca78528ff240c12bebe92f5e4a
Thiago Stahlschmidt <thiago.stahlschmidt@bcb.gov.br>
2021-05-24 18:43:17 -0300

    only build image if it doesn't exist / add hostname to uniquely identify the container

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-docker build -t ibank:sandbox .
-docker run --privileged -dt --shm-size=2G --rm --name ibanksandbox -e DISPLAY=host.docker.internal:0 ibank:sandbox
+[[ -z $(docker images -q ibank:sandbox) ]] && docker build -t ibank:sandbox .
+docker run --privileged -dt --shm-size=2G --rm -h ibanksandbox --name ibanksandbox -e DISPLAY=host.docker.internal:0 ibank:sandbox
 xhost +127.0.0.1 &> /dev/null || true
 docker exec --user bankusr -d ibanksandbox /bin/bash -c browser
