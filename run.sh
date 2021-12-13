67f7d51fd84a512deaa4e691808609ce5b76eefc
Thiago Stahlschmidt <thiago.stahlschmidt@bcb.gov.br>
2021-12-13 19:17:35 -0300

    hello minikube, bye docker desktop

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 [[ -z $(docker images -q ibank:sandbox) ]] && docker build -t ibank:sandbox .
-docker run --privileged -dt --shm-size=2G --rm -h ibanksandbox --name ibanksandbox -e DISPLAY=host.docker.internal:0 ibank:sandbox
-xhost +127.0.0.1 &> /dev/null || true
+X11SERVER=$(minikube ssh ip ro sh default | awk '{ print $3":0" }')
+DOCKERHOST=$(minikube ip)
+docker run --privileged -dt --shm-size=2G --rm -h ibanksandbox --name ibanksandbox -e DISPLAY=${X11SERVER} ibank:sandbox
+xhost +${DOCKERHOST} &> /dev/null || true
 docker exec --user bankusr -d ibanksandbox /bin/bash -c browser
