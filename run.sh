f23f6a91756be16c0ff679e99272207274bf723c
Thiago Stahlschmidt <thiago.stahlschmidt@bcb.gov.br>
2022-05-18 13:51:29 -0300

    atualizacao do chrome

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
-[[ -z $(docker images -q ibank:sandbox) ]] && docker build -t ibank:sandbox .
+[[ -z $(docker images -q ibank:latest) ]] && docker build -t ibank:latest .
 X11SERVER=$(minikube ssh ip ro sh default | awk '{ print $3":0" }')
 DOCKERHOST=$(minikube ip)
-docker run --privileged -dt --shm-size=2G --rm -h ibanksandbox --name ibanksandbox -e DISPLAY=${X11SERVER} ibank:sandbox
+docker run --privileged -dt --shm-size=2G --rm -h ibanksandbox --name ibanksandbox -e DISPLAY=${X11SERVER} ibank:latest
 xhost +${DOCKERHOST} &> /dev/null || true
 docker exec --user bankusr -d ibanksandbox /bin/bash -c browser
